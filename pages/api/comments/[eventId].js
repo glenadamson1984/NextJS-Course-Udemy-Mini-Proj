fc3823e325b961729d64f65153cb1bc6768c4e00
glenadamson1984 <glenadamson1984@gmail.com>
2021-04-06 10:20:35 +0100

    adding mongodb

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
   const eventId = req.query.eventId;
 
   if (req.method === "POST") {
@@ -6,17 +8,29 @@ const handler = (req, res) => {
     const { email, name, text } = req.body;
 
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId
     };
+
+    const client = await MongoClient.connect('mongodb+srv://new:new123@cluster0.fadas.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+    const db = client.db();
+    const result = await db.collection('comments').insertOne(newComment);
+    newComment.id = result.insertedId;
+    client.close();
 
     res.status(201).json({ message: "Added Comment", comment: newComment });
   } else if (req.method === "GET") {
     const dummyList = [{ id: 1, name: "glen", comment: "hello" }];
 
-    res.status(200).json({message: "list of comments", comments: dummyList});
+    const client = await MongoClient.connect('mongodb+srv://new:new123@cluster0.fadas.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+    const db = client.db();
+    const results = await db.collection('comments').find().sort().toArray();
+    client.close();
+
+
+    res.status(200).json({message: "list of comments", comments: results});
   }
 };
 
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,10 +1,16 @@
-const handler = (req, res) => {
+import { MongoClient} from "mongodb";
+
+const handler = async (req, res) => {
     if(req.method === 'POST') {
-        const email = req.body.email;
-        
+        const body = JSON.parse(req.body);
         // in production we would validate the email
 
-        res.status(201).json({message: "email sent successfully"});
+        const client = await MongoClient.connect('mongodb+srv://new:new123@cluster0.fadas.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+        const db = client.db();
+        await db.collection('emails').insertOne({email: body.email});
+        client.close();
+
+        res.status(201).json({message: "email stored successfully"});
     }
     
     
